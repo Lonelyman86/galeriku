476d8f35f2ca06fea4fcb8bf7f35e2d7cdfba6ff
Lonelyman86 <tangerangking01@gmail.com>
2025-06-22 08:30:12 +0700

    Update footer, navbar, dan main.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -7,7 +7,7 @@
               document.write(new Date().getFullYear())
             </script>,
             made with <i class="fa fa-heart"></i> by
-            <a class="font-weight-bold">Ikhsan Alparizki</a>
+            <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Magdalena</a>
           </div>
         </div>
       </div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,7 +8,7 @@
   <link rel="icon" type="image/png" href="{{asset('assets/img/favicons.png')}}">
   <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
   <title>
-    Galeri
+    Galeriku
   </title>
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
